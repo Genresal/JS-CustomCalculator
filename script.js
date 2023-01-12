15d2f05e19d748b1c35bcc084b88487d3a06fbff
vadim <genresal@yandex.ru>
2023-01-12 15:41:56 +0300

    refactor app, remove onclick methods

--- script.js
+++ script.js
@@ -1,14 +1,124 @@
-const displayElement = document.getElementById('displayElement');
-const displayBuffer = document.getElementById('displayBuffer');
-displayElement.value = 0;
+// Variables
 
-var current = 0;
-var firstOperand = 0;
-var secondOperand = 0;
-var operator = null;
-var waitingForSecondOperand = false;
-const commands = [];
-var error = null;
+let displayInput = document.querySelector('#displayInput');
+let displayBuffer = document.querySelector('#displayBuffer');
+
+let buttons = document.querySelectorAll('button');
+let eraseBtn = document.querySelector('#erase');
+let clearBtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+
+displayInput.value = 0;
+
+// Clear
+
+clearBtn.addEventListener("click", () => {
+    clearResults();
+})
+
+// Buttons
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // When number button clicked
+        if(btn.classList.contains('num-btn')) {
+            inputNumber(btn.value)  
+        }    
+        
+        // When operator button clicked
+        if (btn.id.match('op')) {
+            inputOperator(btn.value)
+        }
+        
+        // When clicked button is evaluate button
+        if (btn.id.match('evaluate')) {
+            calculate();
+        }
+        
+        if (btn.value == '+/-') {
+            displayInput.value = -displayInput.value;
+        }
+    })
+})
+
+// Functions
+
+/*function onError(value) {
+  error = value;
+  displayInput.value = value;
+}*/
+
+function inputNumber(value) {
+    /*if (error) {
+      clearResult();
+      error = null;
+    }*/
+    if (value == ".") {
+        if (!displayInput.value.includes('.')) {
+            displayInput.value += value;
+        }
+        
+        return;
+    }
+    
+    if (calculator.waitingForSecondOperand || displayInput.value === 0) {
+        displayInput.value = value;
+        calculator.waitingForSecondOperand = false;
+        calculator.secondOperand = 0;
+        return;
+    }
+    displayInput.value += value;
+}
+
+function inputOperator(operator) {
+    /*if(error) {
+    return;*/
+  if (calculator.firstOperand == 0) {
+      calculator.firstOperand = displayInput.value;
+  } else if (calculator.operator && !calculator.waitingForSecondOperand) {
+    calculate();
+    calculator.firstOperand = displayInput.value;
+  }
+
+  calculator.operator = operator;
+  displayBuffer.value = `${calculator.firstOperand} ${calculator.operator}`;
+  calculator.waitingForSecondOperand = true;
+}
+
+function calculate() {
+      /*if(error) {
+    return;
+  }*/
+    
+    console.log(`${calculator.firstOperand} ${calculator.operator} ${calculator.secondOperand}`);
+    if (calculator.operator == null || calculator.operator == '=') {
+        calculator.firstOperand = displayInput.value;
+        inputOperator('=');
+        return;
+    } else if (calculator.secondOperand != 0) {
+        calculator.firstOperand = displayInput.value;
+    } else {
+        calculator.secondOperand = displayInput.value;
+    }
+
+  displayBuffer.value = `${calculator.firstOperand} ${calculator.operator} ${calculator.secondOperand}`;
+
+  calculator.selectCommand();
+
+  //if(!error) {
+        displayInput.value = calculator.current;
+        calculator.firstOperand = calculator.current;
+        calculator.waitingForSecondOperand = true;
+  //}
+}
+
+function clearResults () {
+    displayBuffer.value = '';
+    displayInput.value = 0;
+    
+    calculator.clear();
+}
+
+// Commands
 
 function add(x, y) {
   return parseFloat(x) + parseFloat(y);
@@ -52,148 +162,63 @@ const DivCommand = function () {
   return new Command(div, mul);
 }
 
-function execute(command) {
-  current = command.execute(firstOperand, secondOperand);
-  commands.push(command);
-}
+// Calculator
 
-function undo () {
-  const command = commands.pop();
-  current = command.undo(current, secondOperand);
-}
-
-function inputOperator(operator) {
-  if(error) {
-    return;
-  } else if (operator == '=') {
-    displayBuffer.value = `${firstOperand} ${operator}`;
-    return;
-  } else if (firstOperand == 0) {
-    firstOperand = displayElement.value;
-  } else if (this.operator && !waitingForSecondOperand) {
-    calculate();
-    firstOperand = displayElement.value;
-  }
-
-  this.operator = operator;
-  displayBuffer.value = `${firstOperand} ${operator}`;
-  waitingForSecondOperand = true;
-}
-
-function inputDigit(value) {
-    if (error) {
-      clearResult();
-      error = null;
-    }
-
-    if (value == '.') {
-      displayElement.value += value;
-      return;
-    }
-
-  if (waitingForSecondOperand || displayElement.value === 0) {
-    displayElement.value = value;
-    waitingForSecondOperand = false;
-    secondOperand = 0;
-  } else {
-    displayElement.value += value;
-  }
-}
-
-function inputDecimalPoint(value) {
-    if (displayElement.value.includes('.') ) {
-        return;
-    }
+const calculator = {
+    current: 0,
+    firstOperand: 0,
+    secondOperand: 0,
+    operator: null,
+    waitingForSecondOperand: false,
+    commands: [],
+    error: null,
     
-    inputDigit(value);
+    execute: function (command) {
+        this.current = command.execute(this.firstOperand, this.secondOperand);
+        this.commands.push(command);
+    },
+    undo: function () {
+        const command = this.commands.pop();
+        this.current = command.undo(current, this.secondOperand);
+    },
+    selectCommand: function () {
+        switch (this.operator) {
+            case "+":
+                this.execute(AddCommand());
+                break
+            case "-":
+                this.execute(SubCommand());
+                break
+            case "*":
+                this.execute(MulCommand());
+                break
+            case "/":
+                this.execute(DivCommand());
+                break
+            default:
+                console.log("Unexpected opration.");
+        }
+    },
+    clear: function () {
+        this.firstOperand = 0;
+        this.secondOperand = 0;
+        this.operator = null;
+        this.waitingForSecondOperand = false;
+    }
 }
 
-function onError(value) {
-  error = value;
-  displayElement.value = value;
-}
-
-function negate() {
-  displayElement.value = -displayElement.value;
-}
-
-function calculate() {
-  if(error) {
-    return;
-  }
-
-    console.log(`${firstOperand} ${operator} ${secondOperand}`);
-    if (operator == null || operator == '=') {
-      firstOperand = displayElement.value;
-      inputOperator('=');
-      return;
-    } else if (secondOperand != 0) {
-    firstOperand = displayElement.value;
-  } else {
-    secondOperand = displayElement.value;
-  }
-
-  displayBuffer.value = `${firstOperand} ${operator} ${secondOperand}`;
-
-  selectCommandByOperator(operator)
-
-  if(!error) {
-    displayElement.value = current;
-    firstOperand = current;
-      waitingForSecondOperand = true;
-  }
-
-}
-
-function backspace() {
-  displayBuffer.value = "";
-  firstOperand = 0;
-  secondOperand = 0;
-  operator = 0;
-}
-
-function clearResult() {
-  displayElement.value = 0;
-  displayBuffer.value = "";
-
-  firstOperand = 0;
-  secondOperand = 0;
-  operator = null;
-  waitingForSecondOperand = false;
-}
-
-function selectCommandByOperator(operator) {
-  switch (operator) {
-    case "+":
-      execute(AddCommand());
-      break
-    case "-":
-      execute(SubCommand());
-      break
-    case "*":
-      execute(MulCommand());
-      break
-    case "/":
-      execute(DivCommand());
-      break
-    case "=":
-        execute(DivCommand());
-        break
-    default:
-      console.log("Unexpected opration.");
-  }
-}
+// Listen keyboard keys
 
 document.addEventListener("keydown", function (event) {
   var key = event.key;
   console.log(key)
   var parsed = parseInt(key);
   if (Number.isInteger(parsed)) {
-    inputDigit(key);
+    inputNumber(key);
   } else {
     switch (key) {
       case '.':
-        inputDecimalPoint(key);
+        inputNumber(key);
         break;
       case '+':
       case '-':
